869673056e70d7fb9f384f8b92c912f160035420
Lloyd H. Meinholz <Lloyd.Meinholz@nielsen.com>
2017-12-18 13:26:44 -0500

    Updated RandomWord.

--- java-random-word/src/main/java/com/javabilities/demo/RandomWord.java
+++ java-random-word/src/main/java/com/javabilities/demo/RandomWord.java
@@ -1,28 +1,28 @@
 package com.javabilities.demo;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+
 public class RandomWord {
-    public Response randomWordHandler(Context context) {
-        LambdaLogger logger = context.getLogger();
+    static final Logger logger = LogManager.getLogger(RandomWord.class);
+
+    public Response handler() {
         // Load the word list
         String fileName = "words.txt";
-        List<String> wordList = new ArrayList<>();
-        logger.log("loading " + fileName);
+        logger.info("loading " + fileName);
         InputStream is = getClass().getResourceAsStream(fileName);
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
-        wordList = br.lines().collect(Collectors.toList());
+        List<String> wordList = br.lines().collect(Collectors.toList());
         try {
             br.close();
             isr.close();
@@ -31,10 +31,10 @@ public class RandomWord {
             e.printStackTrace();
             return null;
         }
-        logger.log("found " + wordList.size() + " words");
+        logger.info("found " + wordList.size() + " words");
         // Return a random word
         String randomWord = wordList.get(new Random().nextInt(wordList.size()));
-        logger.log("random word: " + randomWord);
+        logger.info("random word: " + randomWord);
         return new Response(randomWord);
     }
 }
